2a2f861278edeb8baf5a610bc8c7ab4255fed23f
thesunghwan <thesunghwan@gmail.com>
2016-12-02 18:07:23 +0900

    file combiner done

--- initialize_test.py
+++ initialize_test.py
@@ -6,6 +6,7 @@
 
 import urllib.request
 from pathlib import Path
+import sys, csv
 
 def download_raw_file(language):
     if language == "english":
@@ -19,6 +20,32 @@ def download_raw_file(language):
     else:
         print("error")
 
+def combine_raw_files_to_csv(filename, number_of_lines):
+    #from itertools import izip
+    parallel_corpus = []
+
+    with open("data/europarl-v7.es-en.en") as textfile1, open("data/europarl-v7.es-en.es") as textfile2:
+        i = 0
+        for x, y in zip(textfile1, textfile2):
+            x = x.strip()
+            y = y.strip()
+            line = {
+                "english": x,
+                "spanish": y
+            }
+            parallel_corpus.append(line)
+
+            i = i + 1
+            if i > number_of_lines:
+                break
+
+    with open(filename, 'w') as csvfile:
+        fieldnames = ['english', 'spanish']
+        writer =  csv.DictWriter(csvfile, fieldnames=fieldnames, quoting=csv.QUOTE_NONE)
+        writer.writeheader()
+        for corpus in parallel_corpus:
+            writer.writerow(corpus)
+
 file_english = Path("data/europarl-v7.es-en.en")
 if not file_english.is_file():
     download_raw_file("english")
@@ -26,3 +53,11 @@ if not file_english.is_file():
 file_spanish = Path("data/europarl-v7.es-en.es")
 if not file_spanish.is_file():
     download_raw_file("spanish")
+
+if len(sys.argv) is not 3:
+    print("2 arguments filename, number of lines to extract are needed")
+else:
+    filename = sys.argv[1]
+    number_of_lines = int(sys.argv[2])
+
+    combine_raw_files_to_csv(filename, number_of_lines)
